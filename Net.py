bc218f884fdb2a585e9addfaa9d11e441e9e28d4
wyf0912 <790710534@qq.com>
2018-05-31 10:53:16 +0800

    第一次上传的
    待改进：应该分成删和没删除两个数据集合

--- Net.py
+++ Net.py
@@ -8,7 +8,7 @@ from Data import *
 from torch.autograd import Variable
 
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 class LSTMTagger(nn.Module):
     '''https://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html#lstm-s-in-pytorch'''
@@ -44,7 +44,7 @@ class LSTMTagger(nn.Module):
             # print(type(data_temp.unsqueeze(0)),data_temp.unsqueeze(0).shape,data_temp.unsqueeze(0))
             out, self.hidden = self.lstm(data_temp.unsqueeze(0), self.hidden)
             # print(self.hidden)
-            x = F.relu(self.liner_1(self.hidden[0][0]))
+            x = F.relu(self.liner_1(F.tanh(self.hidden[0][0])))
             x = F.relu(self.liner_2(x))
             x = F.sigmoid(self.ouput_layer(x))
             predict.append(x[0])
@@ -52,7 +52,7 @@ class LSTMTagger(nn.Module):
 
 
 def train():
-    lstm = LSTMTagger(12, 6).cuda()
+    lstm = LSTMTagger(12, 32).cuda()
     transed = TransData(reload=True)
     train_sample = TrainRandomSampler(transed)
     valid_sample = ValidRandomSampler(transed)
@@ -64,16 +64,17 @@ def train():
     )
     valid_loader = DataLoader(
         dataset=transed,
-        batch_size=100,  # 批大小
+        batch_size=60,  # 批大小
         num_workers=5,  # 多线程读取数据的线程数
         sampler=valid_sample
     )
     loss_function = nn.BCEWithLogitsLoss().cuda()
     optimizer = optim.SGD(lstm.parameters(), lr=0.1)
-
+    lstm.load_state_dict(torch.load('./saved_model/lstm 0.816068.pkl'))
     # See what the scores are before training
     # Note that element i,j of the output is the score for tag j for word i.
     # Here we don't need to train, so the code is wrapped in torch.no_grad()
+
     for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
         total_loss = 0
         total_num = 0
@@ -90,17 +91,16 @@ def train():
             total_loss += float(loss)
             loss.backward()
 
-            predict_num = torch.sum(predict>0.5).float()
+            predict_num = torch.sum(predict > 0.5).float()
             activity_num = torch.sum(true_val).float()
-            correct_predict_num = torch.sum((predict>0.5).float()*true_val.float()).float()
-            pre = correct_predict_num / predict_num
-            recall = correct_predict_num / activity_num
-            f1_score = 2 * pre * recall / (pre + recall+0.00001) #加一个数防止nan
-            #print(f1_score)
-            #loss = 1 - f1_score
-            #loss.backward()
+            correct_predict_num = torch.sum((predict > 0.5).float() * true_val.float()).float()
+            # pre = correct_predict_num / predict_num
+            # recall = correct_predict_num / activity_num
+            # f1_score = 2 * pre * recall / (pre + recall+0.00001) #加一个数防止nan
+            # print(f1_score)
+            # loss = 1 - f1_score
+            # loss.backward()
             optimizer.step()
-
 
             total_loss += float(loss)
             total_correct_predict_num += int(correct_predict_num)
@@ -116,61 +116,87 @@ def train():
 '''
             total_num += 1
 
-
         try:
-            pre = total_correct_predict_num/total_predict_num
+            pre = total_correct_predict_num / total_predict_num
         except:
-            pre=0
-        recall =total_correct_predict_num/total_activity_num
-        f1_score = 2*pre*recall/(pre+recall+0.0001)
-        print('epoch:',epoch+1)
+            pre = 0
+        recall = total_correct_predict_num / total_activity_num
+        f1_score = 2 * pre * recall / (pre + recall + 0.0001)
+        print('epoch:', epoch + 1)
         print('train avg loss:', total_loss / total_num)
-        print('train precison:',pre,'recall',recall)
-        print('train F1 score:',f1_score)
+        print('train precison:', pre, 'recall', recall)
+        print('train F1 score:', f1_score)
         torch.save(lstm.state_dict(), './saved_model/lstm %.6f.pkl' % f1_score)
 
-    # See what the scores are after training
+        # See what the scores are after training
 
-        #with torch.no_grad():
+        # with torch.no_grad():
         if 1:
             total_loss = 0
             total_num = 0
             correct_predict_num = 0
             total_activity_num = 0
             total_predict_num = 0
+            total_correct_predict_num = 0
 
-            for i, data in enumerate(train_loader):
+            for i, data in enumerate(valid_loader):
                 lstm.zero_grad()
                 lstm.hidden = lstm.init_hidden()
                 data = Variable(data.float()).cuda()
                 predict, true_val = lstm(data)
-                #loss = loss_function(predict, true_val)
-                #total_loss += float(loss)
-                #loss.backward()
+                # loss = loss_function(predict, true_val)
+                # total_loss += float(loss)
+                # loss.backward()
 
-                predict_num = torch.sum(predict>0.5).float()
+                predict_num = torch.sum(predict > 0.5).float()
                 activity_num = torch.sum(true_val).float()
-                correct_predict_num = torch.sum((predict>0.5).float()*true_val.float()).float()
-                pre = correct_predict_num / predict_num
-                recall = correct_predict_num / activity_num
-                #f1_score = 2 * pre * recall / (pre + recall+0.00001) #加一个数防止nan
+                correct_predict_num = torch.sum((predict > 0.5).float() * true_val.float()).float()
+
+                # f1_score = 2 * pre * recall / (pre + recall+0.00001) #加一个数防止nan
                 loss = 1 - f1_score
 
                 total_loss += float(loss)
                 total_correct_predict_num += int(correct_predict_num)
                 total_predict_num += int(predict_num)
                 total_activity_num += int(activity_num)
+                total_num += 1
+
             try:
-                pre = correct_predict_num/total_predict_num
-                recall =correct_predict_num/total_activity_num
-                f1_score = 2*pre*recall/(pre+recall)
+                pre = float(total_correct_predict_num / total_predict_num)
+                recall = float(total_correct_predict_num / total_activity_num)
+                f1_score = 2 * pre * recall / (pre + recall)
             except:
+                pre = 0
+                recall = 0
                 f1_score = 0
+
             print('valid avg loss:', total_loss / total_num)
-            print('valid precison:',pre,'recall',recall)
-            print('valid F1 score:',f1_score)
+            print('valid precison:', pre, 'recall', recall)
+            print('valid F1 score:', f1_score)
 
 
+def make_predict():
+    lstm = LSTMTagger(12, 32).cuda()
+    data = Data(reload=True)
+    transed = TransData(data=data,reload=False,delete_data=False)
+    data_loader = DataLoader(
+        dataset=transed,
+        batch_size=1,  # 批大小
+        num_workers=1,  # 多线程读取数据的线程数
+    )
+    lstm.load_state_dict(torch.load('./saved_model/lstm 0.816068.pkl'))
+    f = open('result.txt', 'w')
+    for i, data in enumerate(data_loader):
+        id = data[0][0][0]
+        lstm.zero_grad()
+        lstm.hidden = lstm.init_hidden()
+        data[0][0][3] = 30
+        data = Variable(data.float()).cuda()
+        predict, true_val = lstm(data)
+        if int(predict[0] > 0.5):
+            f.write(str(id)+'\n')
+    f.close()
 
 if __name__ == '__main__':
-    train()
+    # train()
+    make_predict()
