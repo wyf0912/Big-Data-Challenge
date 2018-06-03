b40d1907c717c2bfab475167334c994c55a8fa61
wyf0912 <790710534@qq.com>
2018-06-03 12:05:24 +0800

    小改动 增加了多层lstm

--- Net.py
+++ Net.py
@@ -7,7 +7,7 @@ import os
 from Data import *
 from torch.autograd import Variable
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 
 class LSTMTagger(nn.Module):
@@ -16,12 +16,12 @@ class LSTMTagger(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
-
+        self.num_layers = 3
         # self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=1)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=self.num_layers)
 
         # self.embedding_reg = nn.Embedding(12, 32)
         # self.embedding_device = nn.Embedding(3592, hidden_dim - 8)
@@ -30,10 +30,10 @@ class LSTMTagger(nn.Module):
 
         self.trans_layer_1 = nn.Linear(12, 2 * hidden_dim)
         self.trans_layer_2 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
-        #self.trans_layer_3 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
+        # self.trans_layer_3 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
         self.trans_layer_output = nn.Linear(2 * hidden_dim, hidden_dim)
 
-        self.liner_1 = nn.Linear(hidden_dim, 2 * hidden_dim)
+        self.liner_1 = nn.Linear(self.num_layers * hidden_dim, 2 * hidden_dim)
         self.liner_2 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
         self.liner_3 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
         self.liner_4 = nn.Linear(2 * hidden_dim, 2 * hidden_dim)
@@ -41,8 +41,8 @@ class LSTMTagger(nn.Module):
         self.ouput_layer = nn.Linear(2 * hidden_dim, 1)
 
     def init_hidden(self):
-        return (Variable(torch.zeros(1, 1, self.hidden_dim)).cuda(),
-                Variable(torch.zeros(1, 1, self.hidden_dim)).cuda())
+        return (Variable(torch.zeros(self.num_layers, 1, self.hidden_dim)).cuda(),
+                Variable(torch.zeros(self.num_layers, 1, self.hidden_dim)).cuda())
 
     def forward(self, batch_data):
         predict = []
@@ -50,6 +50,9 @@ class LSTMTagger(nn.Module):
         for data in batch_data:
             start_time = int(data[0][2])
             end_time = int(data[0][3])
+
+            #if end_time - start_time > 14:  # 防止数据过长
+            #    start_time = end_time - 14
 
             true_val.append(data[0][1])
             # for i in range(start_time+1, end_time+1):
@@ -69,22 +72,23 @@ class LSTMTagger(nn.Module):
             self,hidden = (1,Variable(torch.zeros(1, 1, self.hidden_dim)).cuda())
             '''
             reg_type = np.zeros(12)
-            #print(int(data[1][0]))
+            # print(int(data[1][0]))
             reg_type[int(data[1][0])] = 1
             reg_type = Variable(torch.Tensor(reg_type)).cuda()
             reg_type = F.relu(self.trans_layer_1(reg_type))
             reg_type = F.relu(self.trans_layer_2(reg_type))
-            #reg_type = F.relu(self.trans_layer_3(reg_type))
+            # reg_type = F.relu(self.trans_layer_3(reg_type))
             reg_type = F.relu(self.trans_layer_output(reg_type))
-            self.hidden = (reg_type.unsqueeze(0).unsqueeze(0),
-                               Variable(torch.zeros(1, 1, self.hidden_dim)).cuda())
+            self.hidden = (torch.cat((reg_type.unsqueeze(0).unsqueeze(0), Variable(torch.zeros(self.num_layers - 1, 1, 32)).cuda())),
+                           Variable(torch.zeros(self.num_layers, 1, self.hidden_dim)).cuda())
             # print(type(data_temp.unsqueeze(0)),data_temp.unsqueeze(0).shape,data_temp.unsqueeze(0))
 
             # out, self.hidden = self.lstm(F.tanh(data_temp.unsqueeze(0)), self.hidden)
             out, self.hidden = self.lstm(data_temp.unsqueeze(0), self.hidden)
 
             # print(self.hidden)
-            x = F.relu(self.liner_1(self.hidden[0][0]))
+
+            x = F.relu(self.liner_1(torch.cat([x[0] for x in self.hidden[0]])))
             x = F.relu(self.liner_2(x))
             x = F.relu(self.liner_3(x))
             x = F.relu(self.liner_4(x))
@@ -106,20 +110,18 @@ def train():
     train_loader = DataLoader(
         dataset=transed,
         batch_size=60,  # 批大小
-        num_workers=5,  # 多线程读取数据的线程数
+        num_workers=1,  # 多线程读取数据的线程数
         sampler=train_sample
     )
     valid_loader = DataLoader(
         dataset=transed,
         batch_size=60,  # 批大小
-        num_workers=5,  # 多线程读取数据的线程数
+        num_workers=1,  # 多线程读取数据的线程数
         sampler=valid_sample
     )
     loss_function = nn.BCEWithLogitsLoss().cuda()
-    optimizer = optim.SGD(lstm.parameters(), lr=0.1)
-    #lstm.load_state_dict(torch.load('./saved_model/lstm_layer4 0.792809.pkl'))
-
-
+    optimizer = optim.SGD(lstm.parameters(), lr=0.3)
+    # lstm.load_state_dict(torch.load('./saved_model/lstm_layer4 0.792809.pkl'))
 
     for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
         total_loss = 0
@@ -129,7 +131,7 @@ def train():
         total_predict_num = 0
 
         for i, data in enumerate(train_loader):
-            #print(i)
+            # print(i)
 
             lstm.zero_grad()
             # lstm.hidden = lstm.init_hidden()
@@ -227,19 +229,18 @@ def make_predict():
         batch_size=1,  # 批大小
         num_workers=1,  # 多线程读取数据的线程数
     )
-    lstm.load_state_dict(torch.load('./saved_model/lstm_layer4+2(input12) 0.796775.pkl'))
+    lstm.load_state_dict(torch.load('./saved_model/lstm_layer4+2(input12) 0.814437.pkl'))
     f = open('result.csv', 'w')
     for i, data in enumerate(data_loader):
         id = data[0][0][0]
         lstm.zero_grad()
-        #lstm.hidden = lstm.init_hidden()
+        # lstm.hidden = lstm.init_hidden()
         data[0][0][3] = 31
         data = Variable(data.float()).cuda()
         predict, true_val = lstm(data)
         if int(predict[0] > 0.5):
             f.write(str(int(id)) + '\n')
     f.close()
-
 
 
 if __name__ == '__main__':
